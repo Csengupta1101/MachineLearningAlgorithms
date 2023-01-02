2720cb743536f29ad5ba89169206b33a8248017a
Chandan Sengupta <Csengupta1101@outlook.com>
2023-01-02 11:12:00 +0530

    Notebook update commit

--- LinearRegression/Car Price Prediction/app.py
+++ LinearRegression/Car Price Prediction/app.py
@@ -5,7 +5,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('Linear_Regression_Model.pkl', 'rb'))
+model = pickle.load(open('Linear_Regression_Model.pkl','rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
